2c99f1c78436a5234cdef5338feae101435e23fc
LukePadawalker <padurariu.luca@gmail.com>
2024-01-23 15:50:06 +0100

    last update

---
+++ js/script.js
@@ -0,0 +1,36 @@
+console.log("Vue ok", Vue);
+
+const { createApp } = Vue;
+
+const app = createApp({
+    name: "carousel",
+    data: () => {
+        destinations;
+    },
+
+    methods: {
+        prevButton(){
+
+            
+        },
+        nextButton(){
+
+        }
+
+
+    }   
+})
+
+app.mount('#carousel');
+
+
+
+/*
+
+facciamo funzionare il carosello:
+ - ogni 3 secondi deve cambiare immagine
+ - se clicco sulla freccia a destra, lui deve andare avanti
+ - se clicco sulla freccia a sinistra, lui deve tornare indietro
+ - se vado con il mouse sulla thumbnail, la thumbnail si deve illuminare e ingrandire
+
+*/
